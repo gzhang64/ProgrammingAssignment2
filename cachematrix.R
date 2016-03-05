d9a1c573c5df50ea3832c30f055323f224c88e80
gzhang <gzhang@udel.edu>
2016-03-05 13:02:15 -0500

    cachemean <- function(x, ...) {         m <- x$getmean()         if(!is.null(m)) {                 message("getting cached data")                 return(m)         }         data <- x$get()         m <- mean(data, ...)         x$setmean(m)         m }

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##################################################
+##          Author: GZ
+##################################################
+
+## makeCacheMatrix: create a matrix, which supports get, set,
+##    setInverse, getInverse
+## cacheSolve: "calculate" inverse of the input by recalling cache
+##    or calculate it on the fly
+
 ## Write a short comment describing this function
-
+## This function contains 4 sub-components
+## --- set()
+## --- get()
+## --- setInverse()
+## --- getInverse()
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(i) inv <<- i
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +35,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)){
+    message("getting cached data: matrix inverse")
+    return (i)
+  }
+  data <- x$get()
+  # matrix inverse here
+  i <- solve(data)
+  x$setInverse(i)
+  i
 }
